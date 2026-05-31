744cfbd18413705ef97466cd47a67c85e82d358f
Raisin69666 <etienne_branchaud@hotmail.com>
2026-05-30 21:20:34 -0400

    Update project to target .NET Standard 2.1 and add ValheimFeedingTroughPlugin class

---
+++ ValheimFeedingTrough/ValheimFeedingTroughPlugin.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data.Common;
+using BepInEx;
+using HarmonyLib;
+
+namespace ValheimFeedingTrough
+{
+    // BepInEx Attributes
+    [BepInPlugin(ModGUID, ModName, ModVersion)]
+    public class ValheimFeedingTroughPlugin : BaseUnityPlugin
+    {
+        // Unique Mod ID (Reverse-Domain)
+        private const string ModGUID = "com.raisin69666.valheimfeedingtrough";
+        private const string ModName = "Valheim Feeding Trough";
+        private const string ModVersion = "1.0.0";
+
+        // Called only once by BepInEx when game starts
+        private void Awake()
+        {
+            // Show message in BepInEx console
+            Logger.LogInfo($"Congrats! The mod {ModName} version {ModVersion} has been succesfully loaded!");
+        }
+    }
+}
